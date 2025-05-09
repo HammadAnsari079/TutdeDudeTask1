b5a3f35ee06190d2c1fe9fbcffc0280c0048ab0b
HammadAnsari079 <167501770+HammadAnsari079@users.noreply.github.com>
2025-05-09 11:32:36 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter any number: "))
+b = int(input("Enter any number: "))
+
+Addition = a+b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division) 
---
+++ 2.py
@@ -0,0 +1,4 @@
+First = input("Enter your first name: ")
+last = input("Enter your last name: ")
+
+print("Hello",First,last,"!"," Welcoem to the Python Program")
